82c1441b43a0324fd7051f7029c7c0c994502f8f
Vika Sh <fuchsjagd666@gmail.com>
2021-12-08 00:23:23 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String test = "test";
+        String test = "test";// write your code here
     }
 }
